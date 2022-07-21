7f50e68f06f39ad510498c76e094c8d64859ca2b
Wilsonmobisa <willexjumah12338@gmail.com>
2022-07-22 00:07:45 +0300

    dd

--- 3.strings.php
+++ 3.strings.php
@@ -1,3 +1,15 @@
 <?php
-echo strlen("Hello world!"); // outputs 12
+echo strlen("Hello world!"); // outputs 12 //returns length
+echo "<br>";
+
+echo str_word_count("Hello world! </br>"); // outputs 2 //returns no.of word 
+echo "<br>";
+
+echo strrev("Hello World");//reverses string
+echo "<br>";
+
+echo strpos("Hello world!", "world"); // outputs 6
+echo "<br>";//searches for a specific text within a string //returns its position
+
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
 ?> 
