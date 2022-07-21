0dbb625e8e42a9d5259ae359f5616bcf65641db0
Wilsonmobisa <willexjumah12338@gmail.com>
2022-07-21 23:59:11 +0300

    dd

--- 2.data-types.php
+++ 2.data-types.php
@@ -3,18 +3,4 @@ $x = 10.365;
 //the function below will return the data type of the variable
 var_dump($x);
 echo "<br>";
-
-//objects
-//a data type which stores data and information on how to process that data.
-
-class Car{
-    function Car(){
-        $this->model="BMW";
-    }
-}
-//create an object
-$herbie=new Car();
-
-//show object properties
-echo $herbie->model;
 ?> 
