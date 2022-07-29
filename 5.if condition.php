aafd6460d4da8494ef4afff3b890d425e9c5cf01
Wilsonmobisa <willexjumah12338@gmail.com>
2022-07-29 19:16:14 +0300

    dd

--- 5.if condition.php
+++ 5.if condition.php
@@ -1,9 +1,9 @@
 <?php
 $t = date("H");
 //H reads the hour in 24hr system
-if($t<"18"){
-    echo("Have a good day");
-} elseif($t<"24"){
+if($t<"10"){
+    echo("Have a good morning");
+} elseif($t<"24")   {
     echo("Have a good night");
 }
 ?>
