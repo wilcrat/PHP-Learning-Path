3c45d77b320c0b0c7530a8cece4f56b72c67bb0d
Wilsonmobisa <willexjumah12338@gmail.com>
2022-07-31 23:36:58 +0300

    dd

--- 6.for loop.php
+++ 6.for loop.php
@@ -1,5 +1,12 @@
 <?php
 for($i=1;$i<=10;$i++){
-    echo "The num is: $i <br>"
+    echo "The num is: $i <br>";
+}
+
+//forearch works only on arrays
+$names = array("Wilson","Json","Goodstein","Hart");
+foreach ($names as $value){
+    echo "$value <br>";
+    echo "names[1]"
 }
 ?>
