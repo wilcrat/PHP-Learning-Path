b0b9fdad0b538fd5da7c57fddaf78eb9d3d0e092
Wilsonmobisa <willexjumah12338@gmail.com>
2022-08-01 13:37:29 +0300

    dd

--- 10.superglobals.php
+++ 10.superglobals.php
@@ -1,3 +1,24 @@
-<?php
-echo $_SERVER['SCRIPT_FILENAME'];
+<!-- $GLOBALS
+$_SERVER
+$_REQUEST
+$_POST
+$_GET
+$_FILES
+$_ENV
+$_COOKIE
+$_SESSION 
+
+//see documentation in w3schools
+-->
+<?php 
+$x = 75; 
+$y = 25;
+ 
+function addition() { 
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
+    //or the other short way check in variables.php
+}
+ 
+addition(); 
+echo $z; 
 ?>
