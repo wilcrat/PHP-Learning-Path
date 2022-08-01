0f4c31b81a52f592fa53716f91fd01c852584372
Wilsonmobisa <willexjumah12338@gmail.com>
2022-08-01 14:00:31 +0300

    dd

--- 10.superglobals.php
+++ 10.superglobals.php
@@ -18,7 +18,14 @@ function addition() {
     $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
     //or the other short way check in variables.php
 }
- 
+
 addition(); 
-echo $z; 
+echo "$z <br>"; 
+
+//$_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.
+echo $_SERVER['SCRIPT_FILENAME'];
+echo "<br>";
+
+//PHP $_REQUEST is used to collect data after submitting an HTML form.
+//see $_resuest.php 
 ?>
